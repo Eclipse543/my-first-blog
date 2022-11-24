c5eb3fc6b52c551ef99c9b1e5e0d7795675d9e98
Eclipse543 <eclipse1458@gamil.com>
2022-11-24 13:33:10 +0545

    My django Project app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Post
+from .models import Student
+from .models import New
+
+admin.site.register(Post)
+admin.site.register(Student)
+admin.site.register(New)
+# Register your models here.
